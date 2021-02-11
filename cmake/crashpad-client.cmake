4d0022eb124cb917e9dddf1b958c4f220a5bee76
OmniBlade <nadia_xy@hotmail.com>
2021-02-11 13:36:41 +0000

    Refactors to tidy up some parts of the cmake script.
    Each library build now moved to its own cmake file.

---
+++ cmake/crashpad-client.cmake
@@ -0,0 +1,49 @@
+# Crashpad client library
+add_library(crashpad_client STATIC)
+
+set_target_properties(crashpad_client PROPERTIES
+    CXX_STANDARD 14
+    POSITION_INDEPENDENT_CODE ON
+    CXX_VISIBILITY_PRESET "hidden"
+    C_VISIBILITY_PRESET "hidden"
+    VISIBILITY_INLINES_HIDDEN ON
+)
+
+target_link_libraries(crashpad_client PRIVATE
+    minichromium
+    crashpad_common
+    crashpad_util
+)
+
+target_sources(crashpad_client PRIVATE
+    ${crashpad_git_SOURCE_DIR}/client/annotation.cc
+    ${crashpad_git_SOURCE_DIR}/client/annotation_list.cc
+    ${crashpad_git_SOURCE_DIR}/client/crash_report_database.cc
+    ${crashpad_git_SOURCE_DIR}/client/crashpad_info.cc
+    ${crashpad_git_SOURCE_DIR}/client/prune_crash_reports.cc
+    ${crashpad_git_SOURCE_DIR}/client/settings.cc
+)
+
+if(APPLE)
+    target_sources(crashpad_client PRIVATE
+        ${crashpad_git_SOURCE_DIR}/client/crash_report_database_mac.mm
+        ${crashpad_git_SOURCE_DIR}/client/crashpad_client_mac.cc
+        ${crashpad_git_SOURCE_DIR}/client/simulate_crash_mac.cc
+    )
+endif()
+
+if (UNIX AND NOT APPLE)
+    target_sources(crashpad_client PRIVATE
+        ${crashpad_git_SOURCE_DIR}/client/crashpad_client_linux.cc
+        ${crashpad_git_SOURCE_DIR}/client/client_argv_handling.cc
+        ${crashpad_git_SOURCE_DIR}/client/crashpad_info_note.S
+        ${crashpad_git_SOURCE_DIR}/client/crash_report_database_generic.cc
+    )
+endif()
+
+if(WIN32)
+    target_sources(crashpad_client PRIVATE
+        ${crashpad_git_SOURCE_DIR}/client/crash_report_database_win.cc
+        ${crashpad_git_SOURCE_DIR}/client/crashpad_client_win.cc
+    )
+endif()
---
+++ cmake/crashpad-common.cmake
@@ -0,0 +1,89 @@
+# Crashpad compile options interface target.
+add_library(crashpad_common INTERFACE)
+
+if(NOT MSVC)
+    target_compile_options(crashpad_common INTERFACE
+        -Wall
+        -Wendif-labels
+        -Werror
+        -Wextra
+        -Wno-missing-field-initializers
+        -Wno-unused-parameter
+        -Wno-implicit-fallthrough
+        -Wsign-compare
+        -Wno-multichar
+        -fno-exceptions
+        -fno-rtti
+        -fno-strict-aliasing
+        -fstack-protector-all
+        -fdata-sections
+        -ffunction-sections
+    )
+else()
+    target_compile_options(crashpad_common INTERFACE
+        $<$<COMPILE_LANGUAGE:CXX>:
+        /FS
+        /W4
+        /WX
+        /Zi
+        /bigobj
+        /wd4100
+        /wd4127
+        /wd4324
+        /wd4351
+        /wd4577
+        /wd4996
+        /wd4201
+        /Zc:inline
+        /d2Zi+
+        >
+    )
+
+    target_link_options(crashpad_common INTERFACE
+        /INCREMENTAL:NO
+        /Debug:FASTLINK
+    )
+endif()
+
+if(WIN32)
+    target_compile_definitions(crashpad_common INTERFACE
+        NOMINMAX
+        UNICODE
+        WIN32_LEAN_AND_MEAN
+        _CRT_SECURE_NO_WARNINGS
+        _HAS_EXCEPTIONS=0
+        _UNICODE
+    )
+
+    target_link_libraries(crashpad_common INTERFACE
+        advapi32.lib
+        winhttp.lib
+        version.lib
+        user32.lib
+        PowrProf.lib
+    )
+else()
+    target_link_libraries(crashpad_common INTERFACE
+      Threads::Threads
+      ${CMAKE_DL_LIBS}
+    )
+endif()
+
+if(APPLE)
+    target_link_options(crashpad_common INTERFACE -Wl,-dead_strip)
+endif()
+
+if(ANDROID)
+    target_link_libraries(crashpad_common INTERFACE -llog)
+endif()
+
+target_compile_definitions(crashpad_common INTERFACE
+    -D_FILE_OFFSET_BITS=64
+    -DCRASHPAD_ZLIB_SOURCE_SYSTEM
+    -DCRASHPAD_LSS_SOURCE_EXTERNAL
+)
+
+target_include_directories(crashpad_common INTERFACE
+    ${crashpad_git_SOURCE_DIR}
+    ${mini_chromium_git_SOURCE_DIR}
+)
